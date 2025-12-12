48995e00c5e881dd44436acb0626a733512d17c2
bogman-code <bogdan.manolescu1@outlook.com>
2025-12-13 00:14:17 +0100

    Add /about page for CI/CD demo

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Home page
 app.get("/", (req, res) => {
-  res.send("Hello from Azure CI/CD! Version 3 🚀");
+  res.send("Hello from Azure CI/CD! Version 4 🎯");
+});
+
+// New /about page – this is our feature change
+app.get("/about", (req, res) => {
+  res.send("This app is deployed automatically from GitHub to Azure using CI/CD.");
 });
 
 app.listen(port, () => {
