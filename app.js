c96a0cd68be00ca05eea6f17996e2b3f146e6b8e
bogman-code <bogdan.manolescu1@outlook.com>
2025-12-12 23:17:10 +0100

    Start the Express server on the specified port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+app.listen(port);
 app.get("/", (req, res) => {
   res.send("Hello from Azure CI/CD! Version 3 🚀");
 });
